dc055eaed4659c9a331a13949c100ff1a054f9d6
Abdullah Ibrahim Dar <139804220+Abdullah374@users.noreply.github.com>
2024-10-18 01:04:16 +0530

    Add files via upload

---
+++ 287.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        hash = {}
+        for num in nums:
+            if num in hash:
+                return num
+            else:
+                hash[num] = True
+        
