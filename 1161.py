a499a52c4691635eb94489f57481beced1254b17
Abdullah Ibrahim Dar <139804220+Abdullah374@users.noreply.github.com>
2024-12-01 14:45:28 +0530

    Add files via upload

---
+++ 1161.py
@@ -0,0 +1,28 @@
+import collections
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        result = []
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                result.append(level)
+        
+        sums = [sum(sublist) for sublist in result]
+        maxSum = max(sums)
+        return sums.index(maxSum) + 1
