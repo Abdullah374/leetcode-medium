563faa237cfb1127fe9ed9a66f8b2c37c5d04e47
Abdullah Ibrahim Dar <139804220+Abdullah374@users.noreply.github.com>
2024-12-01 14:39:13 +0530

    Add files via upload

---
+++ 199.py
@@ -0,0 +1,26 @@
+import collections
+from typing import Optional, List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                result.append(level)
+        rightside = [sublist[-1] for sublist in result]
+        return rightside
